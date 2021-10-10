ae0fd09fbea4e2255cb0abc228035e7fc4ecc834
kadan <kadansk8@gmail.com>
2021-10-10 18:38:47 -0400

    Anotha one

--- src/main/java/me/skarless/listeners/MessageReceivedListener.java
+++ src/main/java/me/skarless/listeners/MessageReceivedListener.java
@@ -17,26 +17,29 @@ public class MessageReceivedListener extends ListenerAdapter {
         if (e.getAuthor().isBot())
             return;
 
-        if(e.getMessage().getContentRaw().charAt(0) == prefix.charAt(0)){
-            for (Command command : TrickordTreat.getInstance().cache.commands) {
-                String[] args = e.getMessage().getContentRaw().split(" ");
+        try{
+            if(e.getMessage().getContentRaw().charAt(0) == prefix.charAt(0)){
+                for (Command command : TrickordTreat.getInstance().cache.commands) {
+                    String[] args = e.getMessage().getContentRaw().split(" ");
 
-                if (Objects.requireNonNull(e.getMember()).hasPermission(command.getPermission())) {
-                    for (String s : command.getCommand()) {
-                        String str = prefix + s;
-                        if (args[0].equalsIgnoreCase(str)) {
-                            try {
-                                TrickordTreat.getInstance().cache.commandQueue.add(new QueuedCommand(e, args, command));
-                                return;
-                            } catch (Exception exception) {
-                                exception.printStackTrace();
+                    if (Objects.requireNonNull(e.getMember()).hasPermission(command.getPermission())) {
+                        for (String s : command.getCommand()) {
+                            String str = prefix + s;
+                            if (args[0].equalsIgnoreCase(str)) {
+                                try {
+                                    TrickordTreat.getInstance().cache.commandQueue.add(new QueuedCommand(e, args, command));
+                                    return;
+                                } catch (Exception exception) {
+                                    exception.printStackTrace();
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            e.getChannel().sendMessage("Unknown command! Try `/help` for help.").queue();
+                e.getChannel().sendMessage("Unknown command! Try `/help` for help.").queue();
+            }
+        }catch(Exception ignore){
         }
     }
 }
